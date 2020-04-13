291f3d692782ed324cc63e489748d4db745ae76d
Doron Sever <doronsever@gmail.com>
2020-04-13 13:25:41 +0300

    added all files neccessary

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'material-drag-drop';
+  disabledDrag = true;
+  timePeriods = [
+    'Bronze age',
+    'Iron age',
+    'Middle ages',
+    'Early modern period',
+    'Long nineteenth century'
+  ];
+
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.timePeriods, event.previousIndex, event.currentIndex);
+    this.disabledDrag = true;
+  }
+  onMouseDown($event: any) {
+    this.disabledDrag = false;
+  }
+
+  stopDragAndDrop() {
+    this.disabledDrag = true;
+  }
 }
